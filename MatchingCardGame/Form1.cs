b8f5b80434db41ecdd67a87e6fdde4261ddb31cd
wisestMe <wyhzest@gmail.com>
2020-10-13 23:25:30 +0100

    make matched pairs visible

--- MatchingCardGame/Form1.cs
+++ MatchingCardGame/Form1.cs
@@ -65,6 +65,14 @@ namespace MatchingCardGame
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
 
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+
+                    return;
+                }
+
                 timer1.Start();
             }
         }
